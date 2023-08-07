bdbdbae3e15f9be265e836aac8ceb43c4e442fce
shusuke-iruma-tea <shusuke.pana@gmail.com>
2023-08-07 21:43:00 +0900

    190/608まで学習完了

---
+++ ch04/top-level-await/server.mjs
@@ -0,0 +1,10 @@
+import { createServer } from "http";
+
+const server = createServer();
+server.on("request", (req, res) => {
+  setTimeout(() => {
+    res.end("hello");
+  }, 100);
+});
+
+server.listen(8000);
